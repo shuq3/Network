aa8edb43e2b40da3606c80d5b315375fdcbbf918
shuq3 <154421897@qq.com>
2017-04-04 16:42:27 +0800

    得到包含网站内部和跨站的用户点击请求

--- http_referer_analyse/referer.py
+++ http_referer_analyse/referer.py
@@ -4,32 +4,60 @@ import socket
 
 f = open('http.pcap')
 pcap = dpkt.pcap.Reader(f)
-mylist = []
+myrequest = {}
+myindex = []
 
 for timestamp, buf in pcap:
     eth = dpkt.ethernet.Ethernet(buf)
-
-    # test is this eth packet contains a ip packet
     if not isinstance(eth.data, dpkt.ip.IP):
         print 'Non IP Packet type not supported %s\n' % eth.data.__class__.__name__
         continue
 
     ip = eth.data
+
     if isinstance(ip.data, dpkt.tcp.TCP):
-        # Set the TCP data
         tcp = ip.data
-        # if the contents is a HTTP request
         try:
             request = dpkt.http.Request(tcp.data)
         except (dpkt.dpkt.NeedData, dpkt.dpkt.UnpackError):
             continue
-        # check if it is the first http request of a new website
+
+        # get http referer
+        myheaders = request.headers
+        if 'accept' in myheaders:
+            mytype = request.headers['accept'];
+            if mytype.find("text/html")!= -1 and mytype.find("application/xhtml+xml")!= -1:
+                myindex.append(timestamp)
+
+        if 'referer' in myheaders :
+            myreferer = myheaders['referer']
+            refererlength = len(myreferer)
+            if myreferer[refererlength-4:] != ".css" and myreferer[refererlength-3:] != ".js":
+                if myheaders['referer'] in myrequest:
+                    myrequest[myheaders['referer']] += 1
+                else:
+                    myrequest[myheaders['referer']] = 1
+
+myrequest = {k: v for k, v in myrequest.items() if v > 1}
+# sort = sorted(myrequest.items(), key=operator.itemgetter(1), reverse = True)
+# print sort
+f.close()
+
+f = open('http.pcap')
+pcap = dpkt.pcap.Reader(f)
+for timestamp, buf in pcap:
+    if len(myindex) > 0 and timestamp == myindex[0]:
+        myindex.pop(0)
+        eth = dpkt.ethernet.Ethernet(buf)
+        ip = eth.data
+        tcp = ip.data
+        request = dpkt.http.Request(tcp.data)
         if request.uri == '/':
-            myheaders = request.headers
-            if 'referer' in myheaders:
-                print 'host: %s\treferer: %s' % \
-                    (myheaders['host'], myheaders['referer'])
-            else:
-                print 'host: %s' % myheaders['host']
+            print request
+        else:
+            urilength = len(request.uri)
+            for keyuri in myrequest.keys():
+                if (keyuri[len(keyuri)-urilength:] == request.uri):
+                    print request
 
 f.close()
